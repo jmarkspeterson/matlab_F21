e2185b6d81f091674b06de22cf68cf1970e990ab
Julia Marks Peterson <peterju2@oregonstate.edu>
2021-10-19 11:31:12 -0700

    week 4

---
+++ HW3_functionwork.m
@@ -0,0 +1,14 @@
+%function TF=cent2fahr(TC)
+%%This function will convert temperature in centigrade to fahrenheit
+%%Created by NAME on DATE
+%%-
+%%TC - (input) temperature in centigrade
+%%TF - (output) temperature in Fahrenheit
+
+%TF=TC*9/5+32;
+
+function y = multGauss(x, [stdev loc])
+
+
+
+
